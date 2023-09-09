5d749a130e7b49139ff53bb634521a054b769f8b
Aryan Saji <aryansaji3103@gmail.com>
2023-09-09 17:18:11 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+  constructor() {
+   /* setTimeout(()=>{
+      this.title="Changed Title";
+    },3000);*/
+    
+  }
 }
